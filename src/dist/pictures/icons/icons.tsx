b7d5cc912d0c80330b14dba2d3ec8eb654db5de4
enndy <91780685+enndylove@users.noreply.github.com>
2024-08-02 14:51:44 +0300

    Export Social Media Icons Array
    Added a module to export an array of URLs for social media icons. This includes icons for GitHub, LeetCode, LinkedIn, and Codewars. The icons are imported from their respective SVG files and exported as a string array.

---
+++ src/dist/pictures/icons/icons.tsx
@@ -0,0 +1,22 @@
+/**
+ * A collection of social media icons.
+ *
+ * This module exports an array of icon URLs for various social media platforms.
+ *
+ * @example
+ * import { icons } from './icons';
+ * console.log(icons); // Output: [githubIcon, leetcodeIcon, linkedinIcon, codewarsIcon]
+ */
+// @ts-ignore
+import githubIcon from './github.svg';
+// @ts-ignore
+import leetcodeIcon from './leetcode.svg';
+// @ts-ignore
+import linkedinIcon from './linkedin.svg';
+// @ts-ignore
+import codewarsIcon from './codewars.svg';
+
+/**
+ * An array of icon URLs for various social media platforms.
+ */
+export const icons: string[] = [githubIcon, leetcodeIcon, linkedinIcon, codewarsIcon];
