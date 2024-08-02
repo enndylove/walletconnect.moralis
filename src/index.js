6568c52d9eb9271556fbc5d1b74cf95de8b4b9ce
enndy <91780685+enndylove@users.noreply.github.com>
2024-08-02 14:52:45 +0300

    Set Up React Application Entry Point

--- src/index.js
+++ src/index.js
@@ -2,9 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+/**
+ * Creates a root element for the React application.
+ *
+ * @param {HTMLElement} element - The HTML element to render the application to.
+ * @returns {ReactDOM.Root} - The root element of the React application.
+ *
+ * Example:
+ * const root = ReactDOM.createRoot(document.getElementById('root'));
+ */
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+/**
+ * Renders the React application to the root element.
+ *
+ * @param {JSX.Element} element - The JSX element to render.
+ *
+ * Example:
+ * root.render(<App />);
+ */
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    /**
+     * Wraps the application in a Strict Mode component, which enables additional checks and warnings.
+     *
+     * @param {JSX.Element} children - The JSX element to wrap.
+     *
+     * Example:
+     * <React.StrictMode>
+     *   <App />
+     * </React.StrictMode>
+     */
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
